bb2f413468041b1d5e176238c8141c07aee48589
Sumit Bera <berasumit611@gmail.com>
2024-08-20 11:21:39 +0530

    updated

--- LogicCode/src/main/java/stringProgram/CharacterCounter.java
+++ LogicCode/src/main/java/stringProgram/CharacterCounter.java
@@ -1,5 +1,17 @@
 package stringProgram;
 //Java Program to count the total number of characters in a string
+//ignore space
 public class CharacterCounter {
+		public static void main(String[] args) {
+			String s="i love java so much";//char->15
+			int i=0,count=0;
+			while(i<s.length()){
+				if(s.charAt(i)!=' ') {
+					count++;
+				}
+				i++;
+			}
+			System.out.println("\""+s+"\""+" has "+count+" charcter");
+		}
 
 }
